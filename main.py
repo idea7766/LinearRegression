7bbed5a48f9b90a35cd7be0dc75e2174a541abac
ke <idea7766@gmail.com>
2017-09-22 20:53:11 +0800

    9/24 commit

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from sklearn import datasets
 import numpy as np
 
-import model
+# import model
 
 # 讀寫 boston 資料，data 和 target 皆為 np array
 boston = datasets.load_boston()
 x = boston.data
 y = boston.target
+print('X shpae:', x.shape)
+print('Y shpae:', y.shape)
 
-model.LinearRegression()
+# model.LinearRegression()
--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def LinearRegression(x, y, lr):
+def LinearRegression(x, y, lr, epoch):
     '''
     # Linear Regression
     ## Basic Concpet
@@ -12,11 +12,16 @@ def LinearRegression(x, y, lr):
     x: traing data X
     y: traing data Y
     lr: learning rate
+    epoch: epoch數
     '''
+    if x.ndim !=2:
+        raise('= =寫錯了啦幹')
     bias = 0
-    w = np.zeros(y.shape[1]) + 1 # initial weight: all 1
+    w = np.ones(y.shape[1]) # initial weight: all 1
+
     for train_x in x:
         pass
+    return b, w
 
 def squre_error(y, y_pred):
     return (y-y_pred)**2
@@ -24,8 +29,21 @@ def squre_error(y, y_pred):
 def y_pred_linear(x, bias, w):
     return bias + np.dot(w, x)
 
-def cal_loss(example_x, example_y):
+def cal_loss(x, y, b, w):
+    return (y[i] - (b + dot(w * x[i]) ) * w[i])
 
-def SGD(x, y, example_x, example_y, w, b):
+def SGD(x, y, lr, b, w):
+    '''
+    # Attribute
+    x: example x
+    y: example y
+    lr: learning rates 
+    # Return 
+    b: constant bias
+    w: weight array
+    '''
+    w_result = np.array(x.shape)
+    b_result = 
     for i in range(x.shape[1]):
-        w[i] = w[i] - 
+        w_result[i] = w[i] - lr * 
+        return b, w
